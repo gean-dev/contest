71acaa4931915e5373cca6d6174b9ba28f76acac
Borworntat Dendurongkul <mastericez890@gmail.com>
2023-10-13 15:52:19 +0700

    fix: ?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  site: "https://gean-dev.github.io/contest",
+  base: "/contest/",
+  integrations: [tailwind(), react()],
 });
